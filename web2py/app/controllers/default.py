4a324c257b16ceada11dcd4e1067e7e5c76599e7
romy584as <romy5082004@gmail.com>
2024-05-06 17:05:15 +0200

    feat: inlogscherm gemaakt met dat de gebruiker alleen maar het `.env` wachtwoord hoeft in te vullen EOD: begin gemaakt aan de het laten zien van de logs

--- web2py/app/controllers/default.py
+++ web2py/app/controllers/default.py
@@ -1,58 +1,75 @@
-# -*- coding: utf-8 -*-
-# -------------------------------------------------------------------------
-# This is a sample controller
-# this file is released under public domain and you can use without limitations
-# -------------------------------------------------------------------------
+import os
 
-# ---- example index page ----
+if False:
+    from ..models.docker_logger_model import *
+
+import bcrypt
+import glob
+
+
+
+def verify_password(password, hashed_password):
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8"))
+
 def index():
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
-
-# ---- API (example) -----
-@auth.requires_login()
-def api_get_user_email():
-    if not request.env.request_method == 'GET': raise HTTP(403)
-    return response.json({'status':'success', 'email':auth.user.email})
-
-# ---- Smart Grid (example) -----
-@auth.requires_membership('admin') # can only be accessed by members of admin groupd
-def grid():
-    response.view = 'generic.html' # use a generic view
-    tablename = request.args(0)
-    if not tablename in db.tables: raise HTTP(403)
-    grid = SQLFORM.smartgrid(db[tablename], args=[tablename], deletable=False, editable=False)
-    return dict(grid=grid)
-
-# ---- Embedded wiki (example) ----
-def wiki():
-    auth.wikimenu() # add the wiki to the menu
-    return auth.wiki() 
-
-# ---- Action for login/register/etc (required for auth) -----
-def user():
     """
-    exposes:
-    http://..../[app]/default/user/login
-    http://..../[app]/default/user/logout
-    http://..../[app]/default/user/register
-    http://..../[app]/default/user/profile
-    http://..../[app]/default/user/retrieve_password
-    http://..../[app]/default/user/change_password
-    http://..../[app]/default/user/bulk_register
-    use @auth.requires_login()
-        @auth.requires_membership('group name')
-        @auth.requires_permission('read','table name',record_id)
-    to decorate functions that need access control
-    also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
+    De indexpagina van de webapplicatie, hier kunnen we de gebruiker laten inloggen met het wachtwoord dat is opgeslagen in het .env-bestand.
+    Dit omdat we alleen de logs willen laten zien aan geautoriseerde gebruikers die het wachtwoord hebben.
     """
-    return dict(form=auth())
+    # Maak een formulier voor het inloggen met een wachtwoordveld (.env)
+    form = SQLFORM.factory(Field('password', 'password', label='Web2py token'))
 
-# ---- action to server uploaded static content (required) ---
-@cache.action()
-def download():
+    # Controleer of het formulier is ingediend
+    if form.process().accepted:
+        # Haal het wachtwoord uit het formulier
+        password = form.vars.password
+
+        # Haal de gehashte wachtwoord van de gebruiker uit de database
+        user = db(db.auth_user.first_name == 'token_user').select().first()
+        hashed_password = user.password if user else None
+
+        # Verifieer het wachtwoord
+        if hashed_password and verify_password(password, hashed_password):
+            # Log de gebruiker in en redirect naar de logs pagina
+            auth.login_user(user)
+            redirect(URL('logs'))
+        else:
+            response.flash = 'Onjuist wachtwoord'
+
+    # Render het formulier
+    return dict(form=form)
+
+
+from pathlib import Path
+
+def logs():
     """
-    allows downloading of uploaded files
-    http://..../[app]/default/download/[filename]
+    De logs pagina van de webapplicatie, hier kunnen we de logs van de Docker container laten zien.
     """
-    return response.download(request, db)
+    # Vind alle .log bestanden in de parent directory van de huidige map en alle submappen
+    log_files = glob.glob('../logs/*.log', recursive=True)
+    logs = []
+    for log_file in log_files:
+        with open(log_file, 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                print(line)
+                logs.append(line)
+
+    return dict(logs=logs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
