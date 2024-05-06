8222ef49f119908e1ffbcb1ffdd4860391e42529
romy584as <romy5082004@gmail.com>
2024-05-06 17:06:07 +0200

    feat: database velden toegevoegd voor latere functionaliteit

---
+++ web2py/app/models/docker_logger_model.py
@@ -0,0 +1,9 @@
+db.define_table("url_opslag",
+                Field("url", "string")
+                )
+db.define_table("zoekterm",
+                Field("zoekterm", "string")
+                )
+db.define_table("log_filter",
+                Field("log_filter", "string")
+                )
