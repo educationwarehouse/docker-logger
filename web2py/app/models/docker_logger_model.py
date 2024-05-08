ee5ba4facc098bf4f90620ad487e33ac82b0b0cd
romy584as <romy5082004@gmail.com>
2024-05-08 14:16:02 +0200

    feat: filters are now getting inserted and commited to the db

--- web2py/app/models/docker_logger_model.py
+++ web2py/app/models/docker_logger_model.py
@@ -7,3 +7,14 @@ db.define_table("zoekterm",
 db.define_table("log_filter",
                 Field("log_filter", "string")
                 )
+
+# insert all the default filters in the log_filter table
+if db(db.log_filter).isempty():
+    print("Inserting default filters")
+    db.log_filter.insert(log_filter="debug")
+    db.log_filter.insert(log_filter="info")
+    db.log_filter.insert(log_filter="warn")
+    db.log_filter.insert(log_filter="error")
+    db.log_filter.insert(log_filter="fatal")
+    db.commit()
+
